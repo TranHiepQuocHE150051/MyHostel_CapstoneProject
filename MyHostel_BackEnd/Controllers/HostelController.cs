10b05c8f8f83dc8cdc8d6b41c45b524bf57c1be4
TrầnHiệpQuốcHE150051 <91178389+TranHiepQuocHE150051@users.noreply.github.com>
2023-04-06 16:58:48 +0700

    Return RoomId in AddResident Response

--- MyHostel_BackEnd/Controllers/HostelController.cs
+++ MyHostel_BackEnd/Controllers/HostelController.cs
@@ -624,14 +624,16 @@ namespace MyHostel_BackEnd.Controllers
                         return Ok(new
                         {
                             IsSuccess = false,
-                            IsInHostel = true
+                            IsInHostel = true,
+                            RoomId= residentInfo.RoomId
                         });
 
                     }
                     return Ok(new
                     {
                         IsSuccess = false,
-                        IsInHostel = true
+                        IsInHostel = true,
+                        RoomId = residentInfo.RoomId
                     });
                 }
                 else
@@ -740,6 +742,16 @@ namespace MyHostel_BackEnd.Controllers
             int distance = response.Routes.First().Legs.First().Distance.Value;
             return distance;
 
+        }
+        private double GetDistance(double longitude, double latitude, double otherLongitude, double otherLatitude)
+        {
+            var d1 = latitude * (Math.PI / 180.0);
+            var num1 = longitude * (Math.PI / 180.0);
+            var d2 = otherLatitude * (Math.PI / 180.0);
+            var num2 = otherLongitude * (Math.PI / 180.0) - num1;
+            var d3 = Math.Pow(Math.Sin((d2 - d1) / 2.0), 2.0) + Math.Cos(d1) * Math.Cos(d2) * Math.Pow(Math.Sin(num2 / 2.0), 2.0);
+
+            return 6376500.0 * (2.0 * Math.Atan2(Math.Sqrt(d3), Math.Sqrt(1.0 - d3)));
         }
         private string replaceString(decimal price)
         {
