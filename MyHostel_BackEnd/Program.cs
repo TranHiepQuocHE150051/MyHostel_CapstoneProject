eb04f0a80748182f0ad90bd7f5c92571ab152883
THANHLTHE150044 <thanhlthe150044@fpt.edu.vn>
2023-04-07 09:47:09 +0700

    fix Json convert

--- MyHostel_BackEnd/Program.cs
+++ MyHostel_BackEnd/Program.cs
@@ -24,6 +24,7 @@ builder.Services.AddControllers().ConfigureApiBehaviorOptions(options =>
 builder.Services.AddControllers().AddJsonOptions(options =>
 {
     options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.Preserve;
+    options.JsonSerializerOptions.NumberHandling = JsonNumberHandling.AllowNamedFloatingPointLiterals;
 });
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(option =>
